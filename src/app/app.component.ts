12e62a1ffc0636be725e711c4936bbaff4183bea
Pravin Singh <pravin130204@gmail.com>
2025-04-03 09:26:37 +0530

    Directives -- Structural Directives -- NgIf, NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Directives';
+  isDisplay:boolean=true;
+  buttonName="Hide";
+  toggle(){
+    this.isDisplay=!this.isDisplay;
+    if(this.isDisplay){
+      this.buttonName="Hide";
+    } else{
+      this.buttonName="Show";
+    }
+  }
+
+  students:{id:number,name:string,age:number}[]=[
+    {
+      id:101,
+      name:"Pravin Singh",
+      age:21
+    },
+    {
+      id:102,
+      name:"Virat Kohli",
+      age:34
+    }
+  ]
 }
